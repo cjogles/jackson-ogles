f0d95a8378fe886be53192462d864f515fefc16b
Jackson Ogles <jackson@builtbyhq.com>
2024-06-26 13:10:49 -0600

    try form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,8 @@
 import { title } from "@/components/primitives";
 import { Input, Textarea } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import emailjs from '@emailjs/browser';
 
 interface IFormInput {
   firstName: string;
@@ -11,38 +12,39 @@ interface IFormInput {
 }
 
 export default function ContactPage() {
-  const { control, handleSubmit, formState: { errors } } = useForm<IFormInput>();
+  const { control, handleSubmit, formState: { errors }, reset } = useForm<IFormInput>({
+    defaultValues: {
+      firstName: "",
+      firstEmail: "",
+      firstNote: ""
+    }
+  });
 
   const onSubmit = async (data: any) => {
-    const formData = new FormData(); // might want to send as application/json instead of multipart/form-data
-    // Loop through the object
-    for (let [key, val] of Object.entries(data)) {
-      // append each item to the formData (converted to JSON strings)
-      formData.append(key, JSON.stringify(val));
+    const templateParams = {
+      from_name: data.firstName,
+      from_email: data.firstEmail,
+      message: data.firstNote
     }
-  //   try {
-  //     const response = await fetch('/api/contact', {
-  //         method: 'post',
-  //         body: formData,
-  //     });
-  //     if (!response.ok) {
-  //         console.log("falling over")
-  //         throw new Error(`response status: ${response.status}`);
-  //     }
-  //     const responseData = await response.json();
-  //     console.log(responseData['message'])
-  //     alert('Message successfully sent');
-  // } catch (err) {
-  //     console.error(err);
-  //     alert("Error, please try resubmitting the form");
-  // }
+    
+    emailjs
+      .send(process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID, process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE_ID, templateParams, {
+        publicKey: "tiiixhf4OR9pwUtaJ"
+      })
+      .then(() => {
+        console.log("email sent successfully");
+        reset();
+      })
+      .catch((error) => {
+        console.error("error:", error);
+      })
   };
 
   return (
     <div className="flex w-full flex-col md:flex-row">
       <h1 className={title({ class: "md:w-5/12"})}>Let's build together.</h1>
-      <div className="flex flex-col mt-5 md:mt-0 md:w-7/12">
-        <div className="mx-20 text-left">
+      <div className="flex flex-col mt-12 md:mt-0 md:w-7/12">
+        <div className="md:mx-20 text-left">
         <form onSubmit={handleSubmit(onSubmit)}>
           <Controller
             rules={{
@@ -97,19 +99,34 @@ export default function ContactPage() {
             control={control}
             defaultValue=""
           />
-          <Button color="primary" type="submit" className="mb-5">Submit</Button>
-          {
-            errors.firstName?.type === "required" && <p role="alert" className="text-red-500" >Name is required</p>
-          }
-          {
-            errors.firstEmail?.type === "required" && <p role="alert" className="text-red-500">Email is required</p>
-          }
-          {
-            errors.firstNote?.type === "required" && <p role="alert" className="text-red-500">Note is required</p>
-          }
-          {
-            errors.firstEmail?.type === "pattern" && <p role="alert" className="text-red-500">Invalid email</p>
-          }
+          <div className="flex md:flex-col">
+            <Button color="primary" type="submit" className="mb-5 max-w-6">Submit</Button>
+            {/* error display mobile */}
+            {
+              (
+                errors.firstName?.type === "required" || 
+                errors.firstEmail?.type === "required" ||
+                errors.firstNote?.type === "required" ||
+                errors.firstEmail?.type === "pattern"
+              ) && <div className="inline-block md:hidden">
+                <p role="alert" className="text-red-500 text-sm inline-block ml-2" >All fields required</p>
+                <p role="alert" className="text-red-500 text-sm  ml-2" >Check email format</p>
+              </div>
+            }
+            {
+              errors.firstName?.type === "required" && <p role="alert" className="text-red-500 hidden md:block" >Name is required</p>
+            }
+            {
+              errors.firstEmail?.type === "required" && <p role="alert" className="text-red-500 hidden md:block">Email is required</p>
+            }
+            {
+              errors.firstNote?.type === "required" && <p role="alert" className="text-red-500 hidden md:block">Note is required</p>
+            }
+            {
+              errors.firstEmail?.type === "pattern" && <p role="alert" className="text-red-500 hidden md:block">Invalid email</p>
+            }
+
+          </div>
         </form>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,17 @@ import NextImage from "next/image";
 
 export default function Home() {
   return (
-    <section className="flex py-8 md:py-10 items-center">
-      <div className="inline-block w-9/12 justify-center pl-3">
+    <section className="flex md:py-10 items-center flex-col md:flex-row align-middle">
+      <div className="inline-block w-9/12 justify-center md:pl-3">
         <h1 className={title()}>Hello! I'm&nbsp;</h1>
         <h1 className={title({ color: "blue" })}>Jackson.&nbsp;</h1>
-        <span className="animate-pulse"><h1 className={title()}>👋</h1></span>
+        <span className="animate-pulse hidden md:inline-block"><h1 className={title()}>👋</h1></span>
         <br />
         <h2 className={subtitle({ class: "mt-4" })}>
           A versatile full stack developer with a passion for creating immersive UI, intuitive UX, and efficient API's 🙌
         </h2>
       </div>
-      <div className="inline-block w-1/2 text-center justify-center px-2 sm:px-10 lg:px-20">
+      <div className="inline-block mt-4 md:mt-0 md:w-1/2 text-center justify-center px-2 sm:px-10 lg:px-20">
         <Image
           as={NextImage}
           priority={true}
@@ -23,7 +23,7 @@ export default function Home() {
           height={240}
           src="/me.jpeg"
           alt="NextUI Album Cover"
-          className="m-5 hover:animate-bounce"
+          className="hover:animate-bounce"
         />
       </div>
     </section>
