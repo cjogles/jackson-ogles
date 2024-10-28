53e7af23407a40115b88a763f0ea1d728824450c
Jackson Ogles <jackson@builtbyhq.com>
2024-10-28 17:14:20 -0600

    dark mode potential fix 1

--- app/_components/theme-switcher.tsx
+++ app/_components/theme-switcher.tsx
@@ -18,14 +18,12 @@ export function ThemeSwitcher() {
     <div style={{display: "flex", cursor: "pointer"}}>
       <button
         onClick={() => setTheme('light')}
-        suppressHydrationWarning
         style={{all: "unset", display: "inline-block", borderRadius: "50%", cursor: "pointer", height: "24px", width: "24px", transition: "all 0.3s ease-in-out 0s", boxShadow: "0 0 50px yellow", backgroundColor: "yellow", border: "1px solid orangered"}}
       />
       <div style={{cursor: "pointer", paddingRight: "10px"}} onClick={() => setTheme('dark')}>
         <button
           onClick={() => setTheme('dark')}
-          suppressHydrationWarning
-          style={{all: "unset", display: "inline-block", borderRadius: "50%", cursor: "pointer", height: "24px", width: "24px", transition: "all 0.3s ease-in-out 0s", boxShadow: "calc(24px / 4) calc(24px / -4) calc(24px / 8)", inset: "#fff", backgroundColor: "transparent", border: "transparent", animation: "n linear 0.5s", rotate: "50deg" }}
+          style={{all: "unset", display: "inline-block", borderRadius: "50%", cursor: "pointer", height: "24px", width: "24px", transition: "all 0.3s ease-in-out 0s", boxShadow: "calc(24px / 4) calc(24px / -4) calc(24px / 8)", inset: "#fff", rotate: "50deg" }}
         />
 
       </div>
